94d2bcdefaf2b42f2c61d88974e109ddd0401e6d
stefseidl <st.seidl@yahoo.de>
2019-03-06 22:24:17 +0100

    Initial commit

--- javascripts/create-graph.js
+++ javascripts/create-graph.js
@@ -15,6 +15,40 @@ function parseData(createGraph){
 
 function createGraph(data){
 	
+	var time = ['x'];
+	var temperature = ["temperature"];
+	var humidity = ["humidity"];
+	
+	for (var i = 0; i < data.length-1; i++){
+		time.push(data[i][0]);
+		temperature.push(data[i][3]);
+		humidity.push(data[i][4]);
+		}
+	
+	var chart = c3.generate({
+		bindto: '#chart',
+		data: {
+			x: 'x',
+			xFormat: '%Y-%m-%d %H:%M:%S', // 'xFormat' can be used as custom format of 'x'
+			columns: [
+				time,
+				temperature,
+				humidity
+			]
+		},
+		axis: {
+			x: {
+				type: 'timeseries',
+				tick: {
+                format: '%Y-%m-%d %H:%M:%S',
+				}
+			}
+		},
+		zoom: {
+			enabled: true
+			}
+	});
+
 }
 
 parseData(createGraph);
